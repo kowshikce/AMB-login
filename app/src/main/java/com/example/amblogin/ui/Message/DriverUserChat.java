61c3ecd1537feb434c128717f9c0d36623b4b8c0
kowshik.lm14@gmail.com <49833043+kowshikjr@users.noreply.github.com>
2020-03-23 21:24:04 +0600

    added driver-user-chat activity for interaction

---
+++ app/src/main/java/com/example/amblogin/ui/Message/DriverUserChat.java
@@ -0,0 +1,15 @@
+package com.example.amblogin.ui.Message;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.amblogin.R;
+
+public class DriverUserChat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_user_chat);
+    }
+}
